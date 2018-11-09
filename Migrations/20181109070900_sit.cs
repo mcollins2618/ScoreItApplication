071e69288ca8780c7f9978851d67cbee1999eb5b
Matt Collins <mcollins2618@gmail.com>
2018-11-09 00:02:12 -0800

    Initial

---
+++ Migrations/20181109070900_sit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AngularASPNETCore2WebApiAuth.Migrations
+{
+    public partial class sit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
